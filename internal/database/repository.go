64e646ab06eeeac7991f1c61c7f138398ab921fa
Asilbek <asilbeksoyib@gmail.com>
2026-05-31 19:07:53 +0500

    SUB-2 Configured Service Layer

--- internal/database/repository.go
+++ internal/database/repository.go
@@ -3,66 +3,79 @@ package database
 import (
 	"context"
 	"errors"
-	"time"
+	"fmt"
 
-	"github.com/Asilbeek1/Subscription-Service/pkg/ctxutil"
-	"github.com/google/uuid"
+	"github.com/Asilbeek1/Subscription-Service/internal/domain"
+	"github.com/jackc/pgx/v5"
 )
 
-var (
-	ErrNotFound = errors.New("Not Found")
-)
-
-type Subscription struct {
-	ServiceName string
-	Price       int32
-	UserID      uuid.UUID
-	StartDate   time.Time
-	EndDate     *time.Time
-}
-
-func (db *DB) Create(ctx context.Context, sub Subscription) error {
-	requestID := ctxutil.GetRequestID(ctx)
+func (db *DB) Create(ctx context.Context, sub domain.CreateSubscription) (*domain.Subscription, error) {
 	query := `
 		INSERT INTO subscriptions
 		(service_name, price, user_id, start_date, end_date)
 		VALUES ($1, $2, $3, $4, $5)
-		`
+		RETURNING id
+	`
 
-	_, err := db.pool.Exec(
-		ctx,
-		query,
+	var id int
+	err := db.pool.QueryRow(ctx, query,
 		sub.ServiceName,
 		sub.Price,
 		sub.UserID,
 		sub.StartDate,
 		sub.EndDate,
-	)
+	).Scan(&id)
+
 	if err != nil {
-		db.log.Error("failed to create subscription",
-			"request_id", requestID,
-			"error", err,
-		)
-		return err
+		return nil, err
 	}
-	return nil
+
+	return &domain.Subscription{
+		ID:          id,
+		ServiceName: sub.ServiceName,
+		Price:       sub.Price,
+		UserID:      sub.UserID,
+		StartDate:   sub.StartDate,
+		EndDate:     sub.EndDate,
+	}, nil
 }
 
-func (db *DB) Update(ctx context.Context, id int, sub Subscription) error {
-	requestID := ctxutil.GetRequestID(ctx)
+func (db *DB) GetById(ctx context.Context, id int) (*domain.Subscription, error) {
+	query := `
+		SELECT id, service_name, price, user_id, start_date, end_date
+		FROM subscriptions
+		WHERE id = $1
+	`
+	var response domain.Subscription
+	err := db.pool.QueryRow(ctx, query, id).Scan(
+		&response.ID,
+		&response.ServiceName,
+		&response.Price,
+		&response.UserID,
+		&response.StartDate,
+		&response.EndDate,
+	)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, domain.ErrNotFound
+		}
+		return nil, err
+	}
+	return &response, nil
+}
+
+func (db *DB) Update(ctx context.Context, id int, sub domain.UpdateSubscription) error {
 	query := `
 		UPDATE subscriptions
 		SET service_name = $1,
-    		price = $2,
-    		user_id = $3,
-    		start_date = $4,
-    		end_date = $5
-		WHERE id = $6;
-		`
+			price = $2,
+			user_id = $3,
+			start_date = $4,
+			end_date = $5
+		WHERE id = $6
+	`
 
-	result, err := db.pool.Exec(
-		ctx,
-		query,
+	result, err := db.pool.Exec(ctx, query,
 		sub.ServiceName,
 		sub.Price,
 		sub.UserID,
@@ -72,15 +85,141 @@ func (db *DB) Update(ctx context.Context, id int, sub Subscription) error {
 	)
 
 	if err != nil {
-		db.log.Error("Error updating the record",
-			"request_id", requestID,
-			"error", err,
-		)
 		return err
 	}
+
 	if result.RowsAffected() == 0 {
-		db.log.Info("subscription not found")
-		return ErrNotFound
+		return domain.ErrNotFound
 	}
+
 	return nil
+}
+
+func (db *DB) Delete(ctx context.Context, id int) error {
+	query := `
+		DELETE FROM subscriptions
+		WHERE id = $1
+	`
+
+	result, err := db.pool.Exec(ctx, query, id)
+	if err != nil {
+		return err
+	}
+
+	if result.RowsAffected() == 0 {
+		return domain.ErrNotFound
+	}
+
+	return nil
+}
+
+func (db *DB) List(ctx context.Context) ([]domain.Subscription, error) {
+	query := `
+		SELECT id, service_name, price, user_id, start_date, end_date
+		FROM subscriptions
+		ORDER BY id ASC
+	`
+
+	rows, err := db.pool.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var result []domain.Subscription
+
+	for rows.Next() {
+		var sub domain.Subscription
+
+		if err := rows.Scan(
+			&sub.ID,
+			&sub.ServiceName,
+			&sub.Price,
+			&sub.UserID,
+			&sub.StartDate,
+			&sub.EndDate,
+		); err != nil {
+			return nil, err
+		}
+
+		result = append(result, sub)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (db *DB) FilterSum(ctx context.Context, filter domain.FilteredSum) ([]domain.Subscription, error) {
+	query, args := buildQueryFilter(filter)
+
+	rows, err := db.pool.Query(ctx, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var result []domain.Subscription
+
+	for rows.Next() {
+		var sub domain.Subscription
+
+		if err := rows.Scan(
+			&sub.ID,
+			&sub.ServiceName,
+			&sub.Price,
+			&sub.UserID,
+			&sub.StartDate,
+			&sub.EndDate,
+		); err != nil {
+			return nil, err
+		}
+
+		result = append(result, sub)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func buildQueryFilter(filter domain.FilteredSum) (string, []interface{}) {
+	query := `
+		SELECT id, service_name, price, user_id, start_date, end_date
+		FROM subscriptions
+		WHERE 1=1
+	`
+
+	args := []any{}
+	argPos := 1
+
+	if filter.UserID != nil {
+		query += fmt.Sprintf(" AND user_id = $%d", argPos)
+		args = append(args, *filter.UserID)
+		argPos++
+	}
+
+	if filter.ServiceName != nil && *filter.ServiceName != "" {
+		query += fmt.Sprintf(" AND service_name = $%d", argPos)
+		args = append(args, *filter.ServiceName)
+		argPos++
+	}
+
+	if filter.From != nil {
+		query += fmt.Sprintf(" AND start_date >= $%d", argPos)
+		args = append(args, *filter.From)
+		argPos++
+	}
+
+	if filter.To != nil {
+		query += fmt.Sprintf(" AND start_date <= $%d", argPos)
+		args = append(args, *filter.To)
+		argPos++
+	}
+
+	return query, args
 }
